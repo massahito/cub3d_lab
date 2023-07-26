5412a5783b42f251ae605cd5d1cb391efa979ab6
libknt <101494707+libknt@users.noreply.github.com>
2023-07-26 13:37:08 +0900

    add commtn file

---
+++ srcs/comment.c
@@ -0,0 +1,86 @@
+/**
+ * attention
+ * Except for the map content, each type of element can be separated by one or more empty line(s).
+*/
+
+/**
+ * Check if the file ends with *.cub
+*/
+
+/**
+ * get_next_line
+*/
+
+/**
+ * North or not. 
+*/
+
+/**
+ * South or not.
+*/
+
+/**
+ * West or not.
+*/
+
+/**
+ * East or not.
+*/
+
+/**
+ * gnl
+ * Azimuth or not.
+ * Otherwise, error.
+*/
+
+/**
+ * texture.
+ * Change to mlx.
+*/
+
+/**
+ * Floor or not.
+*/
+
+/**
+ * Ceiling or not.
+*/
+
+/**
+ * gnl
+ * Floor or Ceiling or not.
+ * Otherwise, error.
+*/
+
+/**
+ * Floor and Ceiling.
+ * Change to mlx.
+*/
+
+/**
+ * Creating a list of maps
+ * gnl 
+ * linear list
+ * per line
+*/
+
+/**
+ * The map must be composed of only 6 possible characters: 0 for an empty space,
+ * 1 for a wall, and N,S,E or W for the player’s start position and spawning
+ * orientation.
+*/
+
+/**
+ * The map must be closed/surrounded by walls, if not the program must return
+ * an error.
+*/
+
+/**
+ * Change map to char **.
+*/
+
+/**
+ * 
+*/
+
+
