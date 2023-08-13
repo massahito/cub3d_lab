39becbb7f65d31f9a7e1bdcd61e189568fb4471d
libknt <101494707+libknt@users.noreply.github.com>
2023-08-13 10:14:29 +0900

    norm map_check

---
+++ srcs/map_check2.c
@@ -0,0 +1,84 @@
+#include "cub3d.h"
+
+void	move_line(char *line, char *newline)
+{
+	while (*line)
+	{
+		if (isspace(*line) == 0)
+			*newline = *line;
+		line++;
+		newline++;
+	}
+}
+
+void	remake_line(t_map_list *map_list, size_t max_size)
+{
+	char	*newline;
+
+	newline = malloc(sizeof(char) * (max_size + 1));
+	if (!newline)
+		malloc_err();
+	memset(newline, C, max_size);
+	newline[max_size] = '\0';
+	move_line(map_list->line, newline);
+	free(map_list->line);
+	map_list->line = newline;
+}
+
+void	make_x_map(t_map_list *map_list)
+{
+	size_t	max_size;
+
+	max_size = max_line_size(map_list);
+	while (map_list)
+	{
+		remake_line(map_list, max_size);
+		map_list = map_list->next;
+	}
+}
+
+char	*make_x_line(char *line, size_t size, char c)
+{
+	char	*newline;
+	size_t	i;
+
+	newline = malloc(sizeof(char) * (size + 1));
+	if (!newline)
+		malloc_err();
+	memset(newline, c, size);
+	newline[size] = '\0';
+	if (line == NULL)
+		return (newline);
+	i = 0;
+	newline++;
+	while (line[i])
+	{
+		newline[i] = line[i];
+		i++;
+	}
+	free(line);
+	return (newline - 1);
+}
+
+void	surround(t_map_list **map_list)
+{
+	size_t		size;
+	t_map_list	*new;
+	t_map_list	*list;
+
+	size = strlen((*map_list)->line) + 2;
+	new = new_map_list(make_x_line(NULL, size, C));
+	new->next = *map_list;
+	(*map_list)->prev = new;
+	*map_list = new;
+	list = new->next;
+	while (list)
+	{
+		list->line = make_x_line(list->line, size, C);
+		list = list->next;
+	}
+	list = map_last(*map_list);
+	new = new_map_list(make_x_line(NULL, size, C));
+	list->next = new;
+	new->prev = list;
+}
---
+++ srcs/map_check3.c
@@ -0,0 +1,77 @@
+#include "cub3d.h"
+
+int	whether_possible_characters(char *line)
+{
+	int	i;
+
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] != ' ' && line[i] != '0' && line[i] != '1' && line[i] != 'N'
+			&& line[i] != 'S' && line[i] != 'E' && line[i] != 'W')
+		{
+			error("Error", "Invalid character: ", &line[i], EXIT_FAILURE);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	whether_player_is_right_or_not(char *line, size_t *player_count)
+{
+	int	i;
+
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] == 'N' || line[i] == 'S' || line[i] == 'E'
+			|| line[i] == 'W')
+			*player_count += 1;
+		i++;
+	}
+	return (0);
+}
+
+int	apply_list(t_map_list *map_list, int (*pfunc)(char *))
+{
+	if (pfunc == NULL)
+		return (0);
+	while (map_list)
+	{
+		if (pfunc(map_list->line))
+			return (1);
+		map_list = map_list->next;
+	}
+	return (0);
+}
+
+int	apply_list2(t_map_list *map_list, size_t *count, int (*pfunc)(char *,
+			size_t *))
+{
+	if (pfunc == NULL)
+		return (0);
+	while (map_list)
+	{
+		if (pfunc(map_list->line, count))
+			return (1);
+		map_list = map_list->next;
+	}
+	return (0);
+}
+
+size_t	max_line_size(t_map_list *map_list)
+{
+	size_t	max;
+	size_t	tmp;
+
+	max = strlen(map_list->line);
+	while (map_list)
+	{
+		tmp = strlen(map_list->line);
+		if (max < tmp)
+			max = tmp;
+		map_list = map_list->next;
+	}
+	return (max);
+}
