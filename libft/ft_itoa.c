bebe0df7f8398084363bd2b821b32d7a482a2074
MasaDevs <110250805+MasaDevs@users.noreply.github.com>
2023-08-17 14:20:06 +0900

    libft

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: keys <keys@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: marai <marai@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 13:22:39 by keys              #+#    #+#             */
-/*   Updated: 2022/11/27 12:31:19 by keys             ###   ########.fr       */
+/*   Updated: 2023/08/17 14:19:43 by marai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,59 +55,3 @@ char	*ft_itoa(int n)
 	itoa_ans(n, str);
 	return (ft_strdup(str));
 }
-
-// static char	*ft_remalloc(char *str, int c)
-// {
-// 	char	*tmp;
-// 	size_t	len;
-
-// 	len = ft_strlen(str);
-// 	tmp = (char *)malloc(sizeof(char) * (len + 2));
-// 	if (!tmp)
-// 		return (NULL);
-// 	*tmp = (char)c;
-// 	ft_strlcat(tmp + 1, str, len + 1);
-// 	free(str);
-// 	return (tmp);
-// }
-
-// static char	*ft_get_num(char *str, long n)
-// {
-// 	int	c;
-
-// 	while (n)
-// 	{
-// 		c = (n % 10) + 48;
-// 		n = n / 10;
-// 		str = ft_remalloc(str, c);
-// 		if (!str)
-// 			return (NULL);
-// 	}
-// 	return (str);
-// }
-
-// char	*ft_itoa(int n)
-// {
-// 	char	*str;
-// 	long	number;
-// 	int		flag;
-
-// 	flag = 0;
-// 	number = (long)n;
-// 	if (number == 0)
-// 		return (ft_strdup("0"));
-// 	if (number < 0)
-// 	{
-// 		flag = 1;
-// 		number = number * -1L;
-// 	}
-// 	str = ft_strdup("");
-// 	if (!str)
-// 		return (NULL);
-// 	str = ft_get_num(str, number);
-// 	if (!str)
-// 		return (NULL);
-// 	if (flag == 1)
-// 		str = ft_remalloc(str, '-');
-// 	return (str);
-// }
