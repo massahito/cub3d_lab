7ad4b697cb4acf469a94fca7013e120c9c2afe36
key-door <101494707+key-door@users.noreply.github.com>
2023-08-17 11:43:20 +0900

    cahgnre error message

--- srcs/read_azimuths_floor_ceil2.c
+++ srcs/read_azimuths_floor_ceil2.c
@@ -5,9 +5,9 @@ int	change_floor_or_ceiling(t_texture_name **texture_name)
 	if ((*texture_name)->floor)
 	{
 		if (check_number_comma((*texture_name)->floor))
-			return (error("Invalid chara: ", "floor", NULL));
+			return (error("Invalid character: ", "floor", NULL));
 		if (check_rgb((*texture_name)->floor))
-			return (error("Invalid chara: ", "floor", NULL));
+			return (error("Invalid character: ", "floor", NULL));
 		add_rgb(&(*texture_name)->f, (*texture_name)->floor);
 	}
 	if ((*texture_name)->ceiling)
@@ -15,7 +15,7 @@ int	change_floor_or_ceiling(t_texture_name **texture_name)
 		if (check_number_comma((*texture_name)->ceiling))
 			return (error("Invalid file: ", "ceiling", NULL));
 		if (check_rgb((*texture_name)->floor))
-			return (error("Invalid chara: ", "ceiling", NULL));
+			return (error("Invalid character:", "ceiling", NULL));
 		add_rgb(&(*texture_name)->c, (*texture_name)->ceiling);
 	}
 	return (0);
